c1ae6ce221ea3ed13f9512052dc45d5368e926b6
Goblin-0-0-7 <mc.goblin@t-online.de>
2025-05-25 15:29:19 +0200

    compilable

--- src/main.cu
+++ src/main.cu
@@ -9,10 +9,10 @@
 #define G = 6.67430 // gravitational const. (10^-11 m³/kgs³)
 
 // Configuration
-float N = 1000; // Number of particles
+#define N 1000 // Number of particles
 float dt = 0.01; // Time step (second?)
 float steps = 1000;
-float p = 16; // Threads per block / Block dimension (how many?)
+#define p 16 // Threads per block / Block dimension (how many?)
 float L = 3; // box width (in meter?)
 
 // ranges for the initial position of the particles
@@ -26,7 +26,7 @@ int posZMin = posXMin;
 int accXMax = 1;
 int accXMin = -accXMax;
 int accYMax = accXMax;
-int accYMin = accXMin,
+int accYMin = accXMin;
 int accZMax = accXMax;
 int accZMin = accXMin;
 // TODO: ranges for the initial weight of the particles
@@ -57,7 +57,7 @@ __device__ float3 bodyBodyInteraction(float4 bi, float4 bj, float3 ai)
 __device__ float3 tile_calculation(float4 myPosition, float3 accel)
 {
     int i;
-    extern __shared__ float4[] shPosition;
+    extern __shared__ float4 shPosition[];
     for (i = 0; i < blockDim.x; i++) {
     accel = bodyBodyInteraction(myPosition, shPosition[i], accel);
     }
@@ -66,7 +66,7 @@ __device__ float3 tile_calculation(float4 myPosition, float3 accel)
 
 __global__ void calculate_forces(void *devX, void *devA)
 {
-  extern __shared__ float4[] shPosition;
+  extern __shared__ float4 shPosition[];
   float4 *globalX = (float4 *)devX;
   float4 *globalA = (float4 *)devA;
   float4 myPosition;
@@ -119,7 +119,7 @@ int main()
 
     // TODO: Run calculation for n time steps
     int grid_dim = N / p; // TODO: probably fix type
-    for (int i = 0; i < steps; i++;) {
+    for (int i = 0; i < steps; i++) {
         calculate_forces<<<grid_dim,p>>>(d_pos, d_acc);
     }
 
